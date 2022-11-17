afbd3aea92a01a15881db28e2e68c17104fd801e
EveK0 <kaylanefs39@gmail.com>
2022-11-17 16:23:08 -0300

    get link to contract address

--- index.js
+++ index.js
@@ -1,31 +1,18 @@
-const pup = require("puppeteer");
-const fs = require("fs");
-const path = require("path");
+import requestPromise from "request-promise";
+import $ from "cheerio";
 const url = "https://bscscan.com/contractsVerified/1?ps=100";
-let count = 1;
 
-(async () => {
-  const browser = await pup.launch({ headless: true });
-  const page = await browser.newPage();
-  console.log("i'm running...");
-  await page.goto(url);
-  const links = await page.$$eval('a[href*="/tx/"]', (el) =>
-    el.map((link) => link.href)
-  );
-  // createDirectory();
-  await browser.close();
-})();
-
-const createDirectory = async () => {
-  const dir = "./files";
-  try {
-    if (!fs.existsSync(dir)) {
-      fs.mkdirSync(dir);
-      console.log("Directory is created.");
-    } else {
-      console.log("Directory already exists.");
+requestPromise(url)
+  .then(function (html) {
+    const link = [];
+    for (let i = 0; i < 100; i++) {
+      link.push(
+        $("#transfers > div > table > tbody > tr > td > a", html)[i].attribs
+          .href
+      );
+      console.log(link[i]);
     }
-  } catch (err) {
+  })
+  .catch(function (err) {
     console.log(err);
-  }
-};
+  });
