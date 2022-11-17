a6471db81b31ef2cd798e243bfac1d08db70e78f
EveK0 <kaylanefs39@gmail.com>
2022-11-17 18:40:01 -0300

    created object

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
 import requestPromise from "request-promise";
-import $ from "cheerio";
+import pkg, { zip } from "pythonic";
+import cheerio from "cheerio";
 const url = "https://bscscan.com/contractsVerified/1?ps=100";
-
 requestPromise(url)
   .then(function (html) {
+    const $ = cheerio.load(html, {
+      xml: {
+        xmlMode: true,
+        withStartIndices: true,
+        withEndIndices: false,
+      },
+    });
     const link = [];
+    const contract_name = [];
+    const compiler = [];
+    const balance = [];
     for (let i = 0; i < 100; i++) {
+      contract_name.push(
+        $("tbody > tr > td:nth-child(2)", html)[i].firstChild.data
+      );
+      compiler.push($("tbody > tr > td:nth-child(3)", html)[i].firstChild.data);
+      balance.push($("tbody > tr > td:nth-child(5)", html)[i].firstChild.data);
+
       link.push(
         $("#transfers > div > table > tbody > tr > td > a", html)[i].attribs
           .href
       );
-      console.log(link[i]);
+    }
+
+    for (const [name, compiler_name, balance_name, addr] of zip(
+      contract_name,
+      compiler,
+      balance, 
+      link
+    )) {
+      const contract_info = {
+        address: addr,
+        name_of_contract: name,
+        compiler: compiler_name,
+        balance: balance_name,
+      };
+      console.log(contract_info);
     }
   })
   .catch(function (err) {
